687972ecc72a06a94b8973bde0de11eefeca3eb4
jereoliver <jere.tienhaara@gmail.com>
2020-01-31 12:21:32 +0200

    ruutu/siirto lisätty
    ruutu/siirto lisätty t.jere

---
+++ shakki/ruutu.cpp
@@ -0,0 +1,32 @@
+#include "ruutu.h"
+
+
+Ruutu::Ruutu(int sarake, int rivi)
+{
+	_sarake = sarake;
+	_rivi = rivi;
+}
+
+
+int Ruutu::getRivi()
+{
+	return _rivi;
+}
+
+
+int Ruutu::getSarake()
+{
+	return _sarake;
+}
+
+
+void Ruutu::setRivi(int rivi)
+{
+	_rivi = rivi;
+}
+
+
+void Ruutu::setSarake(int sarake)
+{
+	_sarake = sarake;
+}
---
+++ shakki/siirto.cpp
@@ -0,0 +1,39 @@
+#include "siirto.h"
+
+
+Siirto::Siirto(Ruutu alkuRuutu, Ruutu loppuRuutu)
+{
+	_alkuRuutu = alkuRuutu;
+	_loppuRuutu = loppuRuutu;
+}
+
+
+Siirto::Siirto(bool lyhytLinna, bool pitkaLinna)
+{
+	_lyhytLinna = lyhytLinna;
+	_pitkaLinna = pitkaLinna;
+}
+
+
+Ruutu Siirto::getAlkuruutu()
+{
+	return _alkuRuutu;
+}
+
+
+Ruutu Siirto::getLoppuruutu()
+{
+	return _loppuRuutu;
+}
+
+
+bool Siirto::onkoLyhytLinna()
+{
+	return _lyhytLinna;
+}
+
+
+bool Siirto::onkoPitkalinna()
+{
+	return _pitkaLinna;
+}
